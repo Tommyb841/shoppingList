63d6f2ed5c23a6c2e065791d03c9e83291867c24
tommyb841 <thomasb841@yahoo.com>
2023-09-29 18:33:16 -0400

    Styled the about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,14 @@
 import React from "react"
 import HomeButton from "../components/HomeButton"
+import NavBar from "../components/NavBar"
+import "../modules/aboutStyles.css"
 export default function about() {
   return (
     <>
+      <NavBar />
+      <h1>About</h1>
       <div className="container">
-        <h1>About</h1>
-        <p>
-          This is to showcase what ive been learning about in React Typescript.
-        </p>
+        <p>This is to showcase what I've been learning in React Typescript.</p>
       </div>
     </>
   )
