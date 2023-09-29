0fb8635be394f5a871eed767dbe69d19d2e9824f
tommyb841 <thomasb841@yahoo.com>
2023-09-29 18:23:48 -0400

    added doge image and image styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,29 @@
 import React from "react"
 import NavBar from "../components/NavBar"
-
-// import item from './modules/item'
-function IndexPage() {
+import { graphql } from "gatsby"
+import Img from "gatsby-image"
+import "../modules/indexStyles.css"
+function IndexPage({ data }) {
   return (
     <>
       <NavBar />
-      <h1>This is the index page to different components</h1>
-      <img src="../image/doge.jpeg" alt="" />
+      <div className="container">
+        <h1>This is the index page to different components</h1>
+        <Img className="doge" fluid={data.file.childImageSharp.fluid} alt="" />
+      </div>
     </>
   )
 }
 
 export default IndexPage
+export const query = graphql`
+  query {
+    file(relativePath: { eq: "doge.jpeg" }) {
+      childImageSharp {
+        fluid(maxWidth: 200) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
