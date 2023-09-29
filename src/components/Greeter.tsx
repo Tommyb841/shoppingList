f37452d693fbad2246f1977b11820d8577bef908
tommyb841 <thomasb841@yahoo.com>
2023-09-29 12:38:40 -0400

    working

--- src/components/Greeter.tsx
+++ src/components/Greeter.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
-import Person from '../modules/GreeterProps'
-import GreeterProps from '../modules/GreeterProps'
-export default function Greeter({person} :GreeterProps): JSX.Element {
+import React, { useRef, useState } from 'react'
+export default function Greeter() {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [enteredName, setEnteredName] = useState<string>("");
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (inputRef.current) {
+      setEnteredName(inputRef.current.value);
+      inputRef.current.value = ("")
+    }
+  };
   return (
-    <div>Hello world from {person}</div>
-    
+    <>
+      <h1>This is the Greeter</h1>
+      <h2>Hello world from {enteredName}</h2>
+      <form onSubmit={handleSubmit}>
+        <input type="text" ref={inputRef} placeholder="enter Name" />
+        <button type="submit"> enter</button>
+      </form>
+    </>
   )
 }
