955211266bc8a84accd073e9c319411ab890968e
tommyb841 <thomasb841@yahoo.com>
2023-09-29 18:23:58 -0400

    added styling

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,11 +3,12 @@ import HomeButton from "../components/HomeButton"
 export default function about() {
   return (
     <>
-      <h1>About</h1>
-      <p>
-        This is to showcase what ive been learning about in React Typescript.
-      </p>
-      <HomeButton />
+      <div className="container">
+        <h1>About</h1>
+        <p>
+          This is to showcase what ive been learning about in React Typescript.
+        </p>
+      </div>
     </>
   )
 }
