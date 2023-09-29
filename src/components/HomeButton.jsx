6a8bd0d0968ffcd63867ccc298f9dafe5f572553
tommyb841 <thomasb841@yahoo.com>
2023-09-29 14:29:50 -0400

    added but wil lbe deleted with a navigation bar

---
+++ src/components/HomeButton.jsx
@@ -0,0 +1,9 @@
+import React from 'react'; 
+import { Link } from 'gatsby' 
+export default function HomeButton() {
+  return (
+        <Link to="/" className="button-style">
+        Home
+      </Link>
+  )
+}
