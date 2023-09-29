6a785d1427917f0dae9298d65878838464d0e254
tommyb841 <thomasb841@yahoo.com>
2023-09-29 12:35:53 -0400

    changed index page to extract greeter component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,12 @@
-import React , {useRef} from 'react'
-import Greeter from '../components/Greeter'
+import React from "react"
+import ShopList from "../components/ShopList"
 // import item from './modules/item'
 function IndexPage() {
-  const inputRef = useRef<HTMLInputElement(null);
-  functin HandleSubmit= (e: React.FormEvent ) => {
-    e.preventDefault();
-    console.log(inputRef.current.value);
-  }
   return (
     <>
-    <form onSubmit={HandleSubmit}></form>
-    <h1>This is the Greeter</h1>
-    <input type="text" ref="inputRef" placeholder="enter Name"/>
-  <Greeter person={inputRef}/>
-    
+      <ShopList />
     </>
   )
 }
-    
+
 export default IndexPage
