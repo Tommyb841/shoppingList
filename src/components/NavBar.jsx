00dbff62b4459dcf5656011e9b4b9c736399b5ec
tommyb841 <thomasb841@yahoo.com>
2023-09-29 17:59:50 -0400

    created NavBar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../modules/NavBarCss.css';
+
+export default function NavBar (){
+    return (
+        <nav>
+                <a href="/">Home </a> 
+                <a href="/about">About </a>
+                <a href="/shoppinglist">Shopping List </a>
+                <a href="/contact">Contact </a>
+        </nav>
+    );
+}
+
